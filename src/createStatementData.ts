76a850cf7e4e950afe2aa28a912a26e298edec58
김동희 <dongheekim@woowahan.com>
2022-02-03 20:50:57 +0900

    feat: 1.8 함수들을 계산기로 옮기기

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -45,65 +45,27 @@ export default function createStatementData(invoice: Invoice, plays: Plays) {
       performance,
       playFor(performance)
     );
-    const result = { ...performance, play: calculator.play };
-    const resultWithAmount = Object.assign(result, {
-      amount: amountFor(result),
-    });
-    const resultWithVolumeCredits = Object.assign(resultWithAmount, {
-      volumeCredits: volumeCreditsFor(resultWithAmount),
-    });
+    const result = {
+      ...performance,
+      play: calculator.play,
+      amount: calculator.amount,
+      volumeCredits: calculator.volumeCredits,
+    };
 
-    return resultWithVolumeCredits;
+    return result;
   }
 
-  function playFor(performance: Performance) {
+  function playFor(performance: Performance): Play {
     return plays[performance.playID];
   }
 
-  function amountFor(performance: Performance) {
-    let result = 0;
-
-    switch (playFor(performance).type) {
-      case "tragedy":
-        result = 40000;
-
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-
-        break;
-      case "comedy":
-        result = 30000;
-
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-
-        result += 300 * performance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(performance).type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(performance: Performance & { play: Play }) {
-    let result = 0;
-    result += Math.max(performance.audience - 30, 0);
-    if (performance.play.type === "comedy") {
-      result += Math.floor(performance.audience / 5);
-    }
-
-    return result;
-  }
-
-  function totalAmount(data: Pick<StatementData, "performances">) {
+  function totalAmount(data: Pick<StatementData, "performances">): number {
     return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
-  function totalVolumeCredits(data: Pick<StatementData, "performances">) {
+  function totalVolumeCredits(
+    data: Pick<StatementData, "performances">
+  ): number {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
@@ -115,5 +77,45 @@ class PerformanceCalculator {
   constructor(performance: Performance, play: Play) {
     this.performance = performance;
     this.play = play;
+  }
+
+  get amount(): number {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+
+        break;
+      case "comedy":
+        result = 30000;
+
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+
+        result += 300 * this.performance.audience;
+        break;
+
+      default:
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits(): number {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if (this.play.type === "comedy") {
+      result += Math.floor(this.performance.audience / 5);
+    }
+
+    return result;
   }
 }
